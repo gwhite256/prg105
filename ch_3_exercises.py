89240c655db2bf29c40fddd426d0e2c1afb8ea94
gwhite256 <70164947+gwhite256@users.noreply.github.com>
2020-08-26 15:40:21 -0500

    Add files via upload

---
+++ ch_3_exercises.py
@@ -0,0 +1,154 @@
+"""
+    Complete all of the TODO directions
+    The number next to the TODO represents the chapter
+    and section in your textbook that explain the required code
+    Your file should compile error free
+    Submit your completed file
+"""
+
+# TODO 3.1 Relational Operators
+print("=" * 10, "Section 3.1 relational operators", "=" * 10)
+# 1) Write a statement using the variables below and a
+#    and a greater-than sign that will evaluate to true.
+#    write a print statement with the statement in parentheses.
+# 2) Write a statement using the variables below that
+#    compares two of the variables to see if they are equal
+#    write a print statement with the statement in parentheses.
+# 3) Write a statement using the variables below that compares
+#    two of the variables below to see if they are not equal
+#    write a print statement with the statement in parentheses.
+# 4) Write a statement using the variables below that uses
+#    the less than or equal to operator
+#    write a print statement with the statement in parentheses.
+
+# variables
+a = 6
+b = 8
+c = 5
+
+# 1 sample answer
+print(a > 3)
+
+# 2
+print(a == 3)
+# 3
+print(a != b)
+# 4
+print(a <= b)
+
+# TODO 3.2 the if else statement
+print("=" * 10, "Section 3.2 if-else", "=" * 10)
+# Add code below to determine if age is greater than or equal to 18
+# Depending on the answer, display the appropriate statement:
+#    "You are old enough to vote" or "You are not old enough to vote"
+# Use the if-else structure
+age = int(input("How old are you? "))
+if age >= 18:
+    print("You are old enough to vote!")
+else:
+    if age <= 18:
+        print("You are not old enough to vote!!")
+
+# TODO 3.3 comparing strings
+print("=" * 10, "Section 3.3 comparing strings", "=" * 10)
+# Complete the code below so that if the user input matches the password
+# it will display "that is correct" otherwise display "that is not correct"
+password = "narwhals"
+user_password = input("Please enter the password:  ")
+
+if user_password == "narwhals":
+    print("That is the password!")
+else:
+    if user_password != "narwhals":
+        print("That is not the password!")
+
+# TODO 3.4 if - elif - else
+print("=" * 10, "Section 3.4 if-elif-else", "=" * 10)
+# Complete the code to accept a number between 1 and 5 from the user
+# and display a roman numeral for that number. If the number entered is
+# not between 1 and 5, have the else statement display "That is not a valid number"
+number = int(input("Please enter a number between 1 and 5: "))
+if number == 5:
+    print("V")
+elif number == 4:
+    print("IV")
+elif number == 3:
+    print("III")
+elif number == 2:
+    print("II")
+elif number == 1:
+    print("I")
+else:
+    print("Unknown Number")
+
+# TODO 3.5 a series of conditions
+print("=" * 10, "Section 3.5 multiple conditions", "=" * 10)
+# Buffet prices are based on the persons age. If the person is a senior
+# citizen (62 or over) , the charge is $9.89. If the person is age 12-
+# 61, the charge is $12.89. If it is a child of under age 3, they eat
+# for free. If the child is between 4 and 11 they are charged $0.99 for
+# each year of age. Complete the code below. SEE SAMPLE PAGE 132
+
+customer_age = int(input("How old is the customer?   "))
+cost = 0  # initializing cost, assign the correct price to this variable
+# Complete the code here to determine the correct cost based on age
+if customer_age >= 61:
+    cost = 12.89
+elif 62 >= customer_age >= 12:
+    cost = 9.89
+elif customer_age <= 3:
+    cost = 0
+elif customer_age == 4:
+    cost = .99
+elif customer_age == 5:
+    cost = 1.98
+elif customer_age == 6:
+    cost = 2.97
+elif customer_age == 7:
+    cost = 3.96
+elif customer_age == 8:
+    cost = 4.95
+elif customer_age == 9:
+    cost = 5.94
+elif customer_age == 10:
+    cost = 6.93
+elif customer_age == 11:
+    cost = 7.92
+
+# Output, correctly formatted -- leave this code to display the result
+print("Your cost for a customer who is " + str(customer_age) + " years old")
+print("is $" + format(cost, ",.2f"))
+
+# TODO 3.5 Logical Operators
+print("=" * 10, "Section 3.5 logical operators", "=" * 10)
+# Using the variables below, combine relational comparisons using logical operators
+# 1) write a print statement using the and operator that will evaluate to true
+# 2) write a print statement using the or operator that will evaluate to true
+# 3) write a print statement using the not operator that will evaluate to true
+d = 10
+e = 10
+f = 12
+
+if d and e == 10:
+    print("True")
+if d or e == 10:
+    print("True")
+if not f:
+    print("True")
+
+# TODO 3.6 Boolean variable
+print("=" * 10, "Section 3.6 boolean variables", "=" * 10)
+# You are programming a baby doll. If the baby doll is tired, it will close its eyes
+# if it is hungry, it will cry. Write code to print  "Eyes open" or "Eyes closed" depending
+# on the tired value. Then print "Crying" or "quiet" depending on the hungry variable
+tired = True
+hungry = False
+
+if tired:
+    print("Eyes Closed")
+if not tired:
+    print("Eyes Open")
+if not hungry:
+    print("Quiet")
+if hungry:
+    print("Crying")
