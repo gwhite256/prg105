bb89d0bb46c50bc593aed6ed84696c5770285528
gwhite256 <70164947+gwhite256@users.noreply.github.com>
2020-09-06 18:16:40 -0500

    Add files via upload

---
+++ 4.2 Practice.py
@@ -0,0 +1,7 @@
+num = 0
+for num in range(11, 1):
+    print(num, "Bottles of beer on the wall")
+    print(num, "Bottles of beer")
+    print("Take one down, pass it around")
+    print(num - 1, "Bottles of beer on the wall")
+    print("")
