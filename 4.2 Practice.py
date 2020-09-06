55cbe01522cd1b6b4909e2638f3444f382d738e5
gwhite256 <70164947+gwhite256@users.noreply.github.com>
2020-09-06 18:27:08 -0500

    Add files via upload

---
+++ 4.2 Practice.py
@@ -0,0 +1,20 @@
+beer = 99
+while beer > 2:
+    print(str(beer), " Bottles of beer on the wall.")
+    print(str(beer), " Bottles of beer.")
+    print("Take one down, pass it around.")
+    print(str(beer - 1), " Bottles of beer on the wall\n\n")
+    beer -= 1
+
+print(str(beer), " Bottles of beer on the wall.")
+print(str(beer), " Bottles of beer.")
+print("Take one down, pass it around.")
+print(str(beer - 1), " Bottle of beer on the wall\n\n")
+beer -= 1
+
+print(str(beer), " Bottle of beer on the wall.")
+print(str(beer), " Bottle of beer.")
+print("Take one down, pass it around.")
+beer -= 1
+print(str(beer), " Bottles of beer on the wall")
+
