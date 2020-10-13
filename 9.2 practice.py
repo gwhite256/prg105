82128ac716c2d006ce5e25c5bd533b1ba8bb88ae
gwhite256 <70164947+gwhite256@users.noreply.github.com>
2020-10-12 19:19:46 -0500

    Delete 9.2 practice.py

--- 9.2 practice.py
+++
@@ -1,8 +0,0 @@
-
-
-def main():
-    numbers = {'one': 'uno', 'two': 'dos', 'three': 'tres', 'four': 'quatro', 'five': 'cinco', 'six': 'seis',
-               'seven': 'siete', 'eight': 'ocho', 'nine': 'nueve', 'ten': 'diez'}
-    for value in numbers:
-        answer = input("How do you say " + value + ' in spanish: ')
-        print(answer)
